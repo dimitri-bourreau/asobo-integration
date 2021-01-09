323745dfee396ae664edc98eed308557de137546
Dimitri Bourreau <contact@dimitribourreau.me>
2021-01-09 17:42:02 +0100

    feat: add basic fade out

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
-window.onload = () => {};
+window.onload = () => {
+  const header = document.getElementsByTagName("header")[0];
+  const { firstElementChild: nav } = header;
+  const navButtons = nav.getElementsByTagName("li");
+
+  Object.keys(navButtons).forEach((key) => {
+    const button = navButtons[key];
+    button.addEventListener("click", async (event) => {
+      const { target } = event;
+      const { classList } = target;
+      const { id: navType } = target.parentNode.parentNode;
+      const isAlreadySelected = classList.contains("nav-selected");
+      const articles = document.getElementById("home-articles");
+
+      if (isAlreadySelected) {
+        return;
+      }
+
+      console.log("navType", navType);
+
+      document
+        .getElementById(navType)
+        .getElementsByClassName("nav-selected")[0]
+        .classList.remove("nav-selected");
+      classList.add("nav-selected");
+
+      articles.style.opacity = 0;
+      setTimeout(() => {
+        articles.style.opacity = 1;
+      }, 200);
+    });
+  });
+};
